9dcfd788a16a0b30b4399484bac0ed5bbf885826
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-03-03 17:05:53 +0900

    [AC] SubmissionID: 6371085 at 2022-03-03 17:05:53 (1646294753)

---
+++ ITP1/ITP1_11_C_6371085_AC.cpp
@@ -0,0 +1,70 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://onlinejudge.u-aizu.ac.jp/status/users/a01sa01to/submissions/1/ITP1_11_C/judge/6371085/C++17
+ * Submitted at: 2022-03-03 17:05:53
+ * Problem URL: https://onlinejudge.u-aizu.ac.jp/problems/ITP1_11_C
+ * Result: AC
+ * Execution Time: 0.0 s
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+struct Dice {
+  int d[6];
+  bool shake(string s, const int* b) {
+    for (char c : s) {
+      bool chk = true;
+      rep(i, 6) {
+        if (d[i] != b[i]) {
+          chk = false;
+          break;
+        }
+      }
+      if (chk) return true;
+      switch (c) {
+        case 'N':
+          swap(d[0], d[1]);
+          swap(d[1], d[5]);
+          swap(d[5], d[4]);
+          break;
+        case 'S':
+          swap(d[0], d[4]);
+          swap(d[4], d[5]);
+          swap(d[5], d[1]);
+          break;
+        case 'E':
+          swap(d[0], d[3]);
+          swap(d[3], d[5]);
+          swap(d[5], d[2]);
+          break;
+        case 'W':
+          swap(d[0], d[2]);
+          swap(d[2], d[5]);
+          swap(d[5], d[3]);
+          break;
+        case 'R':
+          swap(d[1], d[2]);
+          swap(d[2], d[4]);
+          swap(d[4], d[3]);
+          break;
+        case 'L':
+          swap(d[1], d[3]);
+          swap(d[3], d[4]);
+          swap(d[4], d[2]);
+          break;
+      }
+    }
+    return false;
+  }
+};
+
+int main() {
+  Dice dice1;
+  rep(i, 6) cin >> dice1.d[i];
+  int dice2[6];
+  rep(i, 6) cin >> dice2[i];
+  cout << (dice1.shake("RRRRERRRRNRRRRERRRRNRRRRERRRRN", dice2) ? "Yes" : "No") << endl;
+  return 0;
+}
