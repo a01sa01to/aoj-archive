7bb6954d9297a9e2b9b746477ceda4a16b202b4b
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-03-03 11:00:00 +0900

    [AC] SubmissionID: 6370428 at 2022-03-03 11:00:00 (1646272800)

---
+++ ITP1/ITP1_3_A_6370428_AC.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://onlinejudge.u-aizu.ac.jp/status/users/a01sa01to/submissions/1/ITP1_3_A/judge/6370428/C++17
+ * Submitted at: 2022-03-03 11:00:00
+ * Problem URL: https://onlinejudge.u-aizu.ac.jp/problems/ITP1_3_A
+ * Result: AC
+ * Execution Time: 0.0 s
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  rep(_, 1000) cout << "Hello World" << endl;
+  return 0;
+}
