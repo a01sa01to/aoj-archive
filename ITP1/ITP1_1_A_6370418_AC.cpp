af5d9ee8d6509adf77e34727aeeca055b3da7b13
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-03-03 10:55:20 +0900

    [AC] SubmissionID: 6370418 at 2022-03-03 10:55:20 (1646272520)

---
+++ ITP1/ITP1_1_A_6370418_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://onlinejudge.u-aizu.ac.jp/status/users/a01sa01to/submissions/1/ITP1_1_A/judge/6370418/C++17
+ * Submitted at: 2022-03-03 10:55:20
+ * Problem URL: https://onlinejudge.u-aizu.ac.jp/problems/ITP1_1_A
+ * Result: AC
+ * Execution Time: 0.0 s
+ */
+
+#include<iostream>
+int main(){puts("Hello World");}
